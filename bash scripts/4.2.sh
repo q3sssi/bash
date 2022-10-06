fa26c52def4f3eae0bafba28ba5195080c5e8baf
q3sssi <74725509+q3sssi@users.noreply.github.com>
2022-10-06 04:05:50 -1000

    Add files via upload

--- bash scripts/4.2.sh
+++ bash scripts/4.2.sh
@@ -7,8 +7,9 @@ do
 	s1=`echo "$i" | awk -F, '{print $2}'`
 	if [ ! -z $s1 ]
 	then
-		echo "$i" | grep -w "$1"
+		duplicate+=$(echo "$i" | grep -w "$1" | awk -F ':' '{print $4" "$5}')
 	else
 		continue
 	fi
 done
+echo "$duplicate" | tr ' ' '\n' | sort | uniq | awk -F "$1" '{print $1 $2}' | awk -F ',' '{print $1 $2}'
